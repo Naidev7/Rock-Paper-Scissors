accd11ee5e4f6a1c22ea21373f1411d736a58a7a
Naidaly ruiz <naidalyruiz@gmail.com>
2024-01-09 19:51:31 +0100

    design

--- js/main.js
+++ js/main.js
@@ -7,64 +7,54 @@ const resultJugadora = document.querySelector('.js-resultJugadora');
 const resultPc = document.querySelector('.js-pText');
 
 
-function getComparateNumber(pcMovement){
-    const userSelection = selection.ariaValueMax;
-    if(pcMovement === 'piedra' && userSelection === 'piedra'|| pcMovement === 'papel' && userSelection === 'papel' || pcMovement === 'tijeras' && userSelection === 'tijeras'){
-        varText.innerHTML = '¡Empate!';
-        resultJugadora.innerHTML = '0';
-        resultPc.innerHTML = '0';
-    } else{
-        if (pcMovement === 'piedra' && userSelection === 'papel') {
-            varText.innerHTML = '¡Has ganado!';
-            resultJugadora.innerHTML = '1';
-            resultPc.innerHTML = '0';
-            console.log('aquí');
-        }
-        if(pcMovement === 'piedra' && userSelection === 'tijeras'){
-            varText.innerHTML = '¡Has perdido!';
-            resultJugadora.innerHTML = '0';
-            resultPc.innerHTML = ' 1';
-        }
-        if(pcMovement === 'tijeras' && userSelection === 'papel'){
-            varText.innerHTML = '¡Has perdido!';
-            resultJugadora.innerHTML = '0';
-            resultPc.innerHTML = '1';
-        }
-        if(pcMovement === 'tijeras' && userSelection === 'papel'){
-            varText.innerHTML = '¡Has perdido!';
-            resultJugadora.innerHTML = '0';
-            resultPc.innerHTML = '1';
-        }
-    }
-};
-
-
-
-function pcMovement(randomNumber) {
-    let pc = '';
+const pcMovement = (randomNumber)=>{
     if(randomNumber <= 3){
-         pc = 'piedra';
-    } else if (randomNumber >= 7){
-         pc = 'papel';
-    } 
-    else( pc = 'tijeras');
-
-    return pcMovement;
+       return  'piedra';
+   } else if (randomNumber >= 7){
+       return 'papel';
+   } else{return 'tijeras'};
 };
-
-
-
-//Función encargada de generar el número random
 function getRanfomNumber(max) {
     return Math.ceil(Math.random()* max);
+};
+const valuePc = ()=>{
+    const randomNumber = getRanfomNumber(9);
+    const valuePc = pcMovement(randomNumber);
+    return valuePc;
 }
-//Función manejadora del evento click
+
+const playGame = ()=>{
+    const pcValue = valuePc();
+    const userSelection = selection.value;
+     if(userSelection === pcValue){
+        varText.innerHTML  = 'Empate';
+    } else if(userSelection === 'piedra'){
+        if(pcValue === 'tijeras'){
+            varText.innerHTML  = 'has ganado';
+        }else{
+            varText.innerHTML  = 'has perdido';
+        }
+    } else if(userSelection === 'papel'){
+        if(pcValue === 'tijeras'){
+            varText.innerHTML  = 'has perdido';
+        }else{
+            varText.innerHTML  = 'has ganado';
+        }
+    } else{
+        if(pcValue === 'piedra'){
+            varText.innerHTML  = 'has perdido';
+        } else{
+            varText.innerHTML  = 'has ganado';
+        }
+    }
+    console.log(pcValue, userSelection);
+};
+
+
 function handleClick(event) {
     event.preventDefault();
-    const randomNumber = getRanfomNumber(10);
-    const pcMovement = pcMovement();
-    const comparation = getComparateNumber(pcMovement);
-    
-}
+    valuePc();
+    playGame();
+};
 
 btnStart.addEventListener("click", handleClick);
